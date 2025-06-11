2f825af31726f84eb61f9075a15085b8cc61ca70
ElenaMikhaleva <elena.mikhaleva@gmail.com>
2025-06-11 16:59:13 +0300

    to be sure

--- src/main/java/experiments/CssSelectors.java
+++ src/main/java/experiments/CssSelectors.java
@@ -1,4 +1,44 @@
 package experiments;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class CssSelectors {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void selectorsHomePage() {
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+
+        WebElement footer = driver.findElement(By.tagName("footer"));
+
+        WebElement btnLogo = driver.findElement(By.cssSelector("img[src=\"/images/Toolsqa.jpg\"]"));
+        System.out.println(btnLogo.getTagName());
+        System.out.println(btnLogo.getAttribute("src"));
+
+//        WebElement btnCard = driver.findElement(By.cssSelector(".card-up"));
+//        btnCard.click();
+
+        driver.get("https://demoqa.com/elements");
+        driver.manage().window().maximize();
+
+        WebElement btnRadioButton = driver.findElement(By.id("item-2"));
+        btnRadioButton.click();
+
+        pause(10);
+        driver.quit();
+    }
+
+    public void pause (int time) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- src/main/java/experiments/Exceptions.java
+++ src/main/java/experiments/Exceptions.java
@@ -1,4 +1,29 @@
 package experiments;
 
 public class Exceptions {
+    public static void main(String[] args) {
+        String[] strArray = {"str1", "str2", "str3"};
+        uncheckedException(strArray);
+
+    }
+
+    private static void uncheckedException(String[] strArray) {
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(strArray[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("exception index out of bounds");
+        }
+        System.out.println("works");
+    }
+
+    private static void pause() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
