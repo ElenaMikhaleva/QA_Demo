29b405122fa6d58b1bde7ac86195dcea5b7fce36
ElenaMikhaleva <elena.mikhaleva@gmail.com>
2025-06-11 21:35:04 +0300

    changed CssSelectors

--- src/main/java/experiments/CssSelectors.java
+++ src/main/java/experiments/CssSelectors.java
@@ -1,6 +1,7 @@
 package experiments;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,22 +15,41 @@ public class CssSelectors {
         driver.get("https://demoqa.com/");
         driver.manage().window().maximize();
 
+        hideBanner();
+        hideFooter();
+
         WebElement footer = driver.findElement(By.tagName("footer"));
 
         WebElement btnLogo = driver.findElement(By.cssSelector("img[src=\"/images/Toolsqa.jpg\"]"));
         System.out.println(btnLogo.getTagName());
         System.out.println(btnLogo.getAttribute("src"));
 
-//        WebElement btnCard = driver.findElement(By.cssSelector(".card-up"));
-//        btnCard.click();
+        WebElement divCardElements = driver.findElement(By.cssSelector(".card-up"));
+        divCardElements.click();
 
-        driver.get("https://demoqa.com/elements");
-        driver.manage().window().maximize();
-
+        pause(5);
         WebElement btnRadioButton = driver.findElement(By.id("item-2"));
         btnRadioButton.click();
 
-        pause(10);
+        hideFooter();
+
+        WebElement btnRadioButtonYes = driver.findElement(By.cssSelector("label[for=\"yesRadio\"]"));
+        btnRadioButtonYes.click();
+
+        pause(5);
+        driver.navigate().back();
+        driver.navigate().back();
+
+        pause(5);
+        WebElement divCardBookStore = driver.findElement(By.cssSelector("[class=\"card mt-4 top-card\"]:last-child"));
+        divCardBookStore.click();
+
+        pause(5);
+        driver.navigate().back();
+        WebElement divCardWidgets = driver.findElement(By.cssSelector("[class='card mt-4 top-card']:nth-child(4)"));
+        divCardWidgets.click();
+
+        pause(5);
         driver.quit();
     }
 
@@ -39,6 +59,15 @@ public class CssSelectors {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    public void hideBanner() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('#fixedban').style.display='none'");
+    }
+
+    public void hideFooter() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('footer').style.display='none'");
     }
 }
